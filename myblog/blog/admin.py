1d1f50389555b111997b7dfca157591ad1c8951d
hff <422079034@qq.com>
2021-06-05 17:03:11 +0800

    抽取Admin基类

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -5,6 +5,7 @@ from django.utils.html import format_html
 from .models import Post, Category, Tag
 from .adminforms import PostAdminForm
 from myblog.custom_site import custom_site
+from myblog.base_admin import BaseOwnerAdmin
 
 
 # class PostInline(admin.TabularInline):  # StackedInline式样不同
@@ -14,34 +15,25 @@ from myblog.custom_site import custom_site
 
 
 @admin.register(Category, site=custom_site)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(BaseOwnerAdmin):
     # inlines = [PostInline, ]  # 在分类界面增加添加文章的操作
 
     list_display = ('name', 'status', 'is_nav', 'created_time', 'owner', 'post_count')
     fields = ('name', 'status', 'is_nav')
 
     def post_count(self, obj):
-        return obj.post_set.count()
+        return obj.post_set.count()  # post_set：反向查询
 
     post_count.short_description = '文章数量'
 
-    # 自动保存当前登录者为作者
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(CategoryAdmin, self).save_model(request, obj, form, change)  # super里面的参数可省略
-
 
 @admin.register(Tag, site=custom_site)
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(BaseOwnerAdmin):
     list_display = ('name', 'status', 'created_time', 'owner', 'post_count')
     fields = ('name', 'status')
 
     def post_count(self, obj):
         return obj.post_set.count()
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
 class CategoryOwnerFilter(admin.SimpleListFilter):
@@ -61,7 +53,7 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
 
 
 @admin.register(Post, site=custom_site)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(BaseOwnerAdmin):
     form = PostAdminForm
 
     list_display = ('title', 'category', 'status', 'created_time', 'owner', 'operator')
@@ -69,13 +61,13 @@ class PostAdmin(admin.ModelAdmin):
 
     list_filter = [CategoryOwnerFilter]
     search_fields = ['title', 'category__name']
+    save_on_top = True
 
     actions_on_top =True
     actions_on_bottom = True
 
-    save_on_top = True
-
-    exclude = ('owner',)  # 指定不展示的字段
+    # 编辑页面配置
+    # exclude = ('owner',)  # 自动保存，不展示
 
     # 编辑页面展示字段及顺序
     # fields = (
@@ -117,13 +109,6 @@ class PostAdmin(admin.ModelAdmin):
         )
     operator.short_description = '操作'
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super().save_model(request, obj, form, change)
-
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        return qs.filter(owner=request.user)
 
     # 自定义静态资源引入
     # class Media:
